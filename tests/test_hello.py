19bfb7cccd40d93dce8a26aa0d32132db2cf9eff
fr-ukun <61716156+fr-ukun@users.noreply.github.com>
2020-07-23 10:27:39 +0900

    change indent

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -1,10 +1,12 @@
 from hello import hello
 
+
 def test_hello_default(capsys):
     hello()
     out, err = capsys.readouterr()
     assert out == "Hello, world.\n"
 
+    
 def test_hello_with_name(capsys):
     hello("foo")
     hello("bar")
